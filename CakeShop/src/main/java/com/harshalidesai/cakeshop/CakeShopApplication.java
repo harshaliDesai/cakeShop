d8f2f5aefec18d6c743419b43653492fb0bbd525
Harshali Desai <desaiharshali1428@gmail.com>
2024-05-20 18:04:34 +0530

    initial springboot project structure

---
+++ CakeShop/src/main/java/com/harshalidesai/cakeshop/CakeShopApplication.java
@@ -0,0 +1,13 @@
+package com.harshalidesai.cakeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeShopApplication.class, args);
+	}
+
+}
